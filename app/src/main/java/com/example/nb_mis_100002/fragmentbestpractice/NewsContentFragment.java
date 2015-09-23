bb5d4ec82a67b1cf4511ef77ed20657a165b5bda
UTouch2 <mengbochun@expopay.cn>
2015-09-23 16:26:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/nb_mis_100002/fragmentbestpractice/NewsContentFragment.java
@@ -0,0 +1,32 @@
+package com.example.nb_mis_100002.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_news_content, container, false);
+        return view;
+    }
+
+    public void refresh(String newsTitle, String newsContent) {
+        View visibilityLayout = view.findViewById(R.id.visibility_layout);
+        visibilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView) view.findViewById (R.id.news_title);
+        TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+        newsTitleText.setText(newsTitle); // 刷新新闻的标题
+        newsContentText.setText(newsContent); // 刷新新闻的内容
+    }
+
+}
